07d2d819dbb7b89660db912ea3fe128cdcec5088
Gustavo Rondello <gustavorondello99@outlook.com>
2022-02-18 16:28:47 -0300

    Requisito 8 concluído

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     return (
-      <div data-testid="missions">Missoes</div>
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
     );
   }
 }
